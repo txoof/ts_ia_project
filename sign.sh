94fb15683c03522779c695f676d28371cdedbaeb
txoof <aaron.ciuffo@gmail.com>
2021-03-05 10:31:18 +0100

    update signing script

---
+++ sign.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+~/bin/develtools/codesign.py codesign.ini
+
+if [[ $? == 0 ]]; then
+  echo "sign and staple succeeded; pushing update to github"
+  git commit -m "add updated pkg"
+  git push
+else
+  echo "sign and staple failed -- see previous errors"
+  exit 1
+fi
+  
